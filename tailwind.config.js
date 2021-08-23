4363b777ccb4cc453ae364f0644ba956642b4da6
Can Polat <can.polatt@yahoo.com>
2021-08-23 22:27:33 +0300

    🎉 first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1EA5FC",
+        secondary: "#5668D1",
+        background:"#D6DCFF",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
